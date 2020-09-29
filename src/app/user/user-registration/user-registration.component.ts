9efb3a5d9e89e56a1798a8b3a64bae8caf588fc9
Mateo <matteohenao36@gmail.com>
2020-09-28 21:51:59 -0500

    (ANG-1) - Refactoring Code

--- src/app/user/user-registration/user-registration.component.ts
+++ src/app/user/user-registration/user-registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
+import {Gender} from '../user';
 
 @Component({
   selector: 'app-user-registration',
@@ -10,13 +11,15 @@ import { UserService } from '../user.service';
 export class UserRegistrationComponent implements OnInit {
 
   errorMessage: string;
+  successful: boolean;
 
   constructor(private userService: UserService) { }
 
   form = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email])
+    email: new FormControl('', [Validators.required, Validators.email]),
+    gender: new FormControl('', [Validators.required])
   });
 
   get f(){
@@ -27,18 +30,28 @@ export class UserRegistrationComponent implements OnInit {
     this.form.reset({name: '', lastName: '', email: '', gender: '', bornDate: ''});
   }
 
+  getGenders(){
+    return Object.keys(Gender);
+  }
+
   submit(){
     if ( this.form.status === 'VALID' ){
       const user = {
         name: this.form.controls.name.value,
         lastName: this.form.controls.lastName.value,
         email: this.form.controls.email.value,
-        gender: 'MALE',
+        gender: this.form.controls.gender.value,
         bornDate: null
       };
       this.userService.createUser(user).subscribe(
-        () => console.log('Created well'),
-        (error => this.errorMessage = error)
+        () => {
+          console.log('Created well');
+          this.successful = true;
+        },
+        (error => {
+          this.errorMessage = error;
+          this.successful = false;
+        })
       );
     }
     else {
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -2,6 +2,11 @@ export interface IUser{
     email: string;
     name: string;
     lastName: string;
-    gender: string;
+    gender: Gender;
     bornDate: Date;
+}
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE'
 }
